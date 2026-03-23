4a3e80d27a329f23be8c8058fcc52aabb81fd91b
AndreMusic10 <andreams1997@gmail.com>
2026-03-23 18:43:57 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    host: true,       // 🔥 esto es la clave
-    port: process.env.PORT || 10000
+    host: true,
+    port: process.env.PORT || 10000,
+    allowedHosts: ['inventpro.onrender.com']
   }
 })
